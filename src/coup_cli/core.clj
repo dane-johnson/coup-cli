c838978847de691643e2c133865f7b750e4f9f5b
Dane Johnson <dane_johnson@ymail.com>
2018-08-24 16:17:23 -0500

    More game logic

--- src/coup_cli/core.clj
+++ src/coup_cli/core.clj
@@ -1,23 +1,23 @@
 (ns coup-cli.core
   (:gen-class))
 
-(defrecord Action [name cost blockers challengeable identity])
+(defrecord Action [name cost blockers challengeable identity target])
 (def identities ["Duke" "Captian" "Ambassador" "Contessa" "Assassin"])
-(def actions [(Action. "Coup" 7 [] false nil)
-              (Action. "Income" 0 [] false nil)
-              (Action. "Foreign Aid" 0 ["Duke"] false nil)
-              (Action. "Tax" 0 [] true "Duke")
-              (Action. "Exchange" 0 [] true "Ambassador")
-              (Action. "Assassinate" 3 ["Contessa"] true "Assassin")
-              (Action. "Steal" 0 ["Captain" "Ambassador"] true "Captain")])
+(def actions [(Action. "Coup" 7 [] false nil false)
+              (Action. "Income" 0 [] false nil false)
+              (Action. "Foreign Aid" 0 ["Duke"] false nil false)
+              (Action. "Tax" 0 [] true "Duke" false)
+              (Action. "Exchange" 0 [] true "Ambassador" false)
+              (Action. "Assassinate" 3 ["Contessa"] true "Assassin" true)
+              (Action. "Steal" 0 ["Captain" "Ambassador"] true "Captain" true)])
 
 (defrecord Player [number identities revealed-identities coins])
 
 (defn available-actions
   [coins]
-  (if (>= coins 10)
-    (take 1 actions)
-    (filter #(<= (:cost %) coins) actions)))
+  (vec (if (>= coins 10)
+         (take 1 actions)
+         (filter #(<= (:cost %) coins) actions))))
 
 (defn print-player
   [player]
@@ -48,13 +48,39 @@
 (defn make-new-game
   [n-players]
   (let [[player-cards court-deck] (deal-cards (* n-players 2) default-deck)]
-    {:players (map #(Player. %1 (vec %2) [] (if (and (= 2 n-players) (= 0 %)) 1 2)) (range n-players) (partition 2 player-cards))}))
+    {:players (mapv #(Player. %1 (vec %2) [] (if (and (= 2 n-players) (= 0 %)) 1 2)) (range n-players) (partition 2 player-cards))
+     :court-deck court-deck
+     :turn 0}))
+
+(defn alive?
+  [player]
+  (not (empty? (:identities player))))
 
 (defn play-game
   [state]
-  ())
+  (if (> (count (filter alive? (:players state))) 1)
+    ;; There are living players
+    (do
+      (print-player (get (:players state) (:turn state)))
+      (let [player (get (:players state) (:turn state))
+            choice (dec (try (Integer/parseInt (read-line)) (catch Exception _ 0)))
+            targets (concat
+                           (take (:turn state) (:players state))
+                           (drop (inc (:turn state)) (:players state)))]
+        (if-not (< -1 choice (count (available-actions (:coins player))))
+          (do
+            (println "Invalid choice")
+            (recur state))
+          (let [action (get (available-actions (:coins player)) choice)]
+            (if (:target action)
+              (do (println "Choose a target:")
+                  (doseq [[target number] (map vector targets (range))]
+                    (println (str number ". " (:number target))))))))))
+    ;; The game is over, declare last player winner
+    :default
+    ))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (play-game (make-new-game 2)))
